90a29f9d14801640e6d42ae7497f57c77e13ab64
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-12-25 06:16:34 +0100

    Update style

--- style/style.py
+++ style/style.py
@@ -32,7 +32,7 @@ def style():
                 display: flex;
                 justify-content: space-between;
                 align-items: center;
-                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+                box-shadow: 0 4px 8px 0 rgba(0, 230, 250, 0.89), 0 6px 20px 0 rgba(255, 0, 240, 0.7);
                 border-radius: 5px 5px 5px 5px;
                 margin-bottom: 30px;
             }
